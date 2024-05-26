909e93ea1a919466563eb0d82e1e4158cccb686e
sabujkumarjena <sabuj.kumar.jena@gmail.com>
2024-05-27 03:51:51 +0530

    TCPChannel incomplete

---
+++ src/main/scala/com/allevite/tcp/TCPChannel.scala
@@ -0,0 +1,61 @@
+package com.allevite.tcp
+
+import fs2.*
+import cats.effect.*
+import cats.implicits.*
+
+import java.nio.ByteBuffer
+import java.nio.channels.SocketChannel
+
+/**
+ * A channel to write and read from a single connection.
+ */
+trait TCPChannel[F[_]]:
+  /**
+   * The bytes sent by the client through this channel.
+   */
+  def stream: Stream[F, Byte]
+
+  /**
+   * Sends (writes) the given byte array through this channel.
+   */
+  def write(bytes: Array[Byte]): F[Unit]
+
+  /**
+   * Closes the channel.
+   */
+  def close(): F[Unit]
+
+object TCPChannel:
+  /**
+   * Creates a TCP channel from a given java.nio.SocketChannel.
+   *
+   * Both reads and writes are done in batches of the specified size.
+   */
+  def fromSocketChannel[F[_] : Sync](
+                                      socketChannel: SocketChannel,
+                                      bufferSize: Int = 4096
+                                    ): TCPChannel[F] =
+    new TCPChannel[F]:
+      override def stream: Stream[F, Byte] =
+        /**
+         * TODO #3
+         *
+         * Read one chunk of size 'bufferSize' from 'socketChannel'.
+         */
+        val readChunk: F[Chunk[Byte]] =
+          ???
+
+        Stream.evalUnChunk(readChunk).repeat
+
+      override def write(bytes: Array[Byte]): F[Unit] =
+        bytes.sliding(bufferSize, bufferSize).toList.traverse_ { chunk =>
+          Sync[F].blocking {
+            val byteBuffer: ByteBuffer = ByteBuffer.allocate(chunk.length)
+            byteBuffer.put(chunk)
+            byteBuffer.flip()
+            socketChannel.write(byteBuffer)
+          }
+        }
+          
+      override def close(): F[Unit] = Sync[F].blocking(socketChannel.close())
